11ce3f83ff0d7ee454be5a939dd76d6aaae733eb
sh4869 <nobuk4869@gmail.com>
2017-01-20 21:55:55 +0900

    Using helper class in commands

--- lib/src/commands/install.dart
+++ lib/src/commands/install.dart
@@ -3,14 +3,25 @@ import 'dart:io';
 
 import 'package:pubspec/pubspec.dart';
 import 'package:args/command_runner.dart';
-import 'package:pub_client/pub_client.dart' as pub_client;
-import 'package:pub_semver/pub_semver.dart' as semver;
+
+import '../helper/dependency_reference_helper.dart';
+import '../helper/pubspec_helper.dart';
 
 class InstallCommand extends Command {
   String get name => "install";
   String get description => "Install package.";
   String get invocation =>
       "${runner.executableName} $name [options] <package name>";
+
+  Function get onPubSpecError => (e) {
+        if (e is FileSystemException) {
+          print(e.message);
+          exit(1);
+        } else {
+          throw e;
+        }
+      };
+
   InstallCommand() {
     argParser.addOption("git",
         help: """Install package from git.  parameter example:
@@ -30,62 +41,23 @@ class InstallCommand extends Command {
     }
 
     String package_name = argResults.rest[0];
-    PubSpec pubspec;
-    try {
-      pubspec = await PubSpec.load(Directory.current);
-    } catch (e) {
-      print(e);
-      exit(1);
-    }
+    PubSpec pubspec = await PubSpecHelper.loadPubspec(onPubSpecError);
 
     DependencyReference reference;
     if (argResults["git"] != null) {
-      reference = _getGitReference(argResults["git"], package_name);
+      reference = ReferenceHelper.getGitReference(argResults["git"]);
     } else if (argResults["path"] != null) {
-      print("$name : " + argResults["path"]);
-      reference = new PathReference(argResults["path"]);
+      reference = ReferenceHelper.getPathReference(argResults["path"]);
     } else {
-      reference = await _getHostedReference(package_name);
+      reference = await ReferenceHelper.getHostedReference(package_name);
     }
-    var dependencyMap = pubspec.allDependencies;
-    dependencyMap[package_name] = reference;
-    pubspec = pubspec.copy(dependencies: dependencyMap);
-    try {
-      await pubspec.save(Directory.current);
-    } catch (e) {
-      print(e);
-    }
+    print(ReferenceHelper.getDependencyInfo(reference, package_name));
+
+    pubspec = pubspec.copy(
+        dependencies: ReferenceHelper.addReference(
+            package_name, reference, pubspec.allDependencies));
+
+    await PubSpecHelper.savePubspec(pubspec, onPubSpecError);
     exit(0);
-  }
-
-  GitReference _getGitReference(String url, name) {
-    if (url.contains("#")) {
-      List<String> params = url.split("#");
-      String log =
-          name + ": git\n" + "  url : " + params[0] + "\n  ref : " + params[1];
-      print(log);
-      return new GitReference(params[0], params[1]);
-    } else {
-      print(name + " : " + url);
-      return new GitReference(url, null);
-    }
-  }
-
-  Future<HostedReference> _getHostedReference(String name) async {
-    print("Getting Package info from pub.dartlang.org...");
-    pub_client.PubClient client = new pub_client.PubClient();
-    pub_client.FullPackage package;
-    try {
-      package = await client.getPackage(name);
-    } on pub_client.HttpException catch (e) {
-      switch (e.status) {
-        case 404:
-          print("Error: $name is not found on pub.dartlang.org.");
-          exit(1);
-      }
-    }
-    print(name + " : " + package.latest.version);
-    return new HostedReference(
-        new semver.Version.parse(package.latest.version));
   }
 }
--- lib/src/commands/uninstall.dart
+++ lib/src/commands/uninstall.dart
@@ -4,11 +4,26 @@ import 'dart:io';
 import 'package:pubspec/pubspec.dart';
 import 'package:args/command_runner.dart';
 
+import '../helper/pubspec_helper.dart';
+import '../helper/dependency_reference_helper.dart';
+
 class UninstallCommand extends Command {
   String get name => "uninstall";
+
   String get description => "unsinsall package.";
+
   String get invocation => "${runner.executableName} $name <package name>";
+
   UninstallCommand() {}
+
+  Function get onPubSpecError => (e) {
+        if (e is FileSystemException) {
+          print(e.message);
+          exit(1);
+        } else {
+          throw e;
+        }
+      };
 
   Future<Null> run() async {
     if (argResults.rest.length < 1) {
@@ -17,23 +32,17 @@ class UninstallCommand extends Command {
       exit(1);
     }
     String package_name = argResults.rest[0];
-    PubSpec spec;
-    try {
-      spec = await PubSpec.load(Directory.current);
-    } catch (e) {
-      print(e);
-      exit(1);
-    }
-    var dependencyMap = spec.allDependencies;
-    if (dependencyMap.containsKey(package_name)) {
-      dependencyMap.remove(package_name);
-      spec = spec.copy(dependencies: dependencyMap);
-      await spec.save(Directory.current);
-      print("$package_name is uninstalled.");
+    PubSpec spec = await PubSpecHelper.loadPubspec(onPubSpecError);
+    var result =
+        ReferenceHelper.removeReference(package_name, spec.allDependencies);
+    if (result.isPresent) {
+      spec = spec.copy(dependencies: result.value);
+      await PubSpecHelper.savePubspec(spec, onPubSpecError);
+      print(package_name + " is uninstalled.");
+      exit(0);
     } else {
-      print("$package_name is not installed.");
+      print(package_name + " is not installed.");
       exit(1);
     }
-    exit(0);
   }
 }
